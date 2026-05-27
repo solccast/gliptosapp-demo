ef28ecc8d150addb86bb58e8d2ed211dac58ace9
Sol Angela Chilaca Castro <solaccastro@gmail.com>
2026-05-27 01:05:47 -0300

    add navegacion

--- app/src/main/java/com/example/gliptosapp/ui/home/InitFragment.kt
+++ app/src/main/java/com/example/gliptosapp/ui/home/InitFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.gliptosapp.R
 import com.example.gliptosapp.databinding.FragmentInitBinding
 
 class InitFragment : Fragment() {
@@ -17,6 +19,19 @@ class InitFragment : Fragment() {
     ): View {
         _binding = FragmentInitBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+
+        binding.btnColeccion.setOnClickListener {
+            navController.navigate(R.id.colectionFragment)
+        }
+
+        binding.btnAjustes.setOnClickListener {
+            navController.navigate(R.id.settingsFragment)
+        }
     }
 
 }
