ec0da441423f4cf1189a050227dea85ff65472f7
Sol Angela Chilaca Castro <solaccastro@gmail.com>
2026-06-01 18:00:33 -0300

    fix vista de la colección de fosiles = proto. Uso de las variables declaradas en values

--- app/src/main/java/com/example/gliptosapp/ui/colection/ColectionFragment.kt
+++ app/src/main/java/com/example/gliptosapp/ui/colection/ColectionFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gliptosapp.databinding.FragmentColectionBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -42,7 +43,7 @@ class ColectionFragment : Fragment() {
             findNavController().navigate(action)
         }
 
-        binding.listaFosiles.layoutManager = LinearLayoutManager(requireContext())
+        binding.listaFosiles.layoutManager = GridLayoutManager(requireContext(), 2)
         binding.listaFosiles.adapter = adapter
 
         colectionViewModel.fosiles.observe(viewLifecycleOwner) { lista ->
--- app/src/main/java/com/example/gliptosapp/ui/colection/FosilAdapter.kt
+++ app/src/main/java/com/example/gliptosapp/ui/colection/FosilAdapter.kt
@@ -1,21 +1,23 @@
 package com.example.gliptosapp.ui.colection
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gliptosapp.data.Fosil
 import com.example.gliptosapp.databinding.ItemFosilBinding
+import com.example.gliptosapp.databinding.ItemFosilDescBinding
 
 class FosilAdapter(
     private var lista: List<Fosil>,
     private val onDetalleClick: (Fosil) -> Unit
 ) : RecyclerView.Adapter<FosilAdapter.FosilViewHolder>() {
 
-    class FosilViewHolder(val binding: ItemFosilBinding) :
+    class FosilViewHolder(val binding: ItemFosilDescBinding) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FosilViewHolder {
-        val binding = ItemFosilBinding.inflate(
+        val binding = ItemFosilDescBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
@@ -29,11 +31,11 @@ class FosilAdapter(
         // TODO: hay que reemplazar este código feo por menos if
         with(holder.binding) {
             nombreFosil.text = fosil.nombre
-            estadoFosil.text = if (fosil.descubierto) "Descubierto" else "No descubierto"
+            //estadoFosil.text = if (fosil.descubierto) "Descubierto" else "No descubierto"
             imagenFosil.setImageResource(fosil.obtenerImagen())
             btnDetalle.isEnabled = fosil.descubierto
-
-            // ♿ accesibilidad
+            btnDetalle.visibility = if (!fosil.descubierto) View.GONE else View.VISIBLE
+            // accesibilidad clau clau
             root.contentDescription =
                 "Fósil ${fosil.nombre}, " +
                         if (fosil.descubierto) "descubierto" else "no descubierto"
