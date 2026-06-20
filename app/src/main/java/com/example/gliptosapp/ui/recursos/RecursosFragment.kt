432ae436a58333dab4ab8a64b11cdd097aaf67d1
Sol Angela Chilaca Castro <solaccastro@gmail.com>
2026-06-20 00:30:37 -0300

    feat: add links a los recursos

--- app/src/main/java/com/example/gliptosapp/ui/recursos/RecursosFragment.kt
+++ app/src/main/java/com/example/gliptosapp/ui/recursos/RecursosFragment.kt
@@ -1,9 +1,13 @@
 package com.example.gliptosapp.ui.recursos
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.fragment.findNavController
 import com.example.gliptosapp.databinding.FragmentRecursosBinding
 import com.example.gliptosapp.ui.BaseFragment
@@ -30,8 +34,37 @@ class RecursosFragment: BaseFragment() {
             findNavController().popBackStack()
         }
 
+        binding.btnDescargar.setOnClickListener {
+            openUrl(RecursosLinks.LIBRO_CAMINANDO)
+        }
+
+        binding.btnVer.setOnClickListener {
+            openUrl(RecursosLinks.DOCUMENTAL_CAMINANDO)
+        }
+        binding.btnVerCaminandoTierrasNuevas.setOnClickListener {
+            openUrl(RecursosLinks.LIBRO_TIERRAS_NUEVAS)
+        }
+
+        // Redes sociales
+        binding.btnInstagram.setOnClickListener {
+            openUrl(RecursosLinks.INSTAGRAM)
+        }
+        binding.btnFacebook.setOnClickListener {
+            openUrl(RecursosLinks.FACEBOOK)
+        }
+        binding.btnYoutube.setOnClickListener {
+            openUrl(RecursosLinks.YOUTUBE)
+        }
+        binding.btnWeb.setOnClickListener {
+            openUrl(RecursosLinks.WEB)
+        }
+
     }
 
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
